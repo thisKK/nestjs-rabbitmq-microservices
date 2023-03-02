49f05361a7464594663e16a2a6ba734bfcc8c0f4
thiskk <sonicrat12@gmail.com>
2023-03-03 01:25:04 +0700

    :tada: add api create order

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.dto';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly orderRepository: OrdersRepository) {}
+  async createOrder(request: CreateOrderRequest) {
+    return this.orderRepository.create(request);
   }
 }
